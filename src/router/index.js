850894f4902b0e0e7ce24ffce86dcd79d1730e08
fuyunlong <18667184527@163.com>
2021-11-27 07:46:22 +0800

    feat(src):新增推出功能，加入代码格式

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,11 +12,25 @@ const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+// next()  强制跳转next('/login)
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
